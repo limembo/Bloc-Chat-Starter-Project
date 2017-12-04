06091a15189440728607748efa700628ea09cc87
limembo <lmembis@gmail.com>
2017-12-04 06:54:21 -0500

    Completed user story for checkpoint 5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,34 @@
 (function() {
-  function config($locationProvider, $stateProvider) {
-           $locationProvider
-               .html5Mode({
-                   enabled: true,
-                   requireBase: false
-                });
+     function config($locationProvider, $stateProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+              });
 
-           $stateProvider
-               .state('home', {
-                   url: '/',
-                   controller: 'HomeCtrl as home',
-                   templateUrl: '/templates/home.html'
-               });
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
 
-               $stateProvider
-                .state('modal', {
-                  url: '/',
-                  controller: 'ModalCtrl as modal',
-                  templateUrl: '/templates/room-modal.html'
-                });
-       }
+          $stateProvider
+             .state('modal', {
+                 url: '/',
+                 controller: 'ModalCtrl as modal',
+                 templateUrl: '/templates/room-modal.html'
+             });
+
+          $stateProvider
+             .state('cookies', {
+                 url: '/',
+                 controller: 'CookiesCtrl as cookies',
+                 templateUrl: '/templates/new-user-modal.html'
+             });
+     }
 
      angular
-         .module('blocChat', ['ui.router', 'firebase','ui.bootstrap'])
+         .module('blocChat', ['ui.router', 'firebase','ui.bootstrap', 'ngCookies'])
          .config(config);
  })();
---
+++ app/scripts/services/BlocChatCookies.js
@@ -0,0 +1,19 @@
+(function() {
+  function BlocChatCookies($cookies, $uibModal) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+            $uibModal.open({
+                animation: true,
+                templateUrl: '/templates/new-user-modal.html',
+                controller: 'CookiesCtrl as cookies',
+                windowClass: 'modal-window',
+                backdrop: 'static'
+
+            });
+        }
+    }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
+})();
