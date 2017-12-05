781f26143c2a233f029bfb310fce7dd5e6ed349e
limembo <lmembis@gmail.com>
2017-12-05 02:36:59 -0500

    Completed user story for checkpoint 6

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,10 @@
 (function() {
-    function HomeCtrl($scope, $uibModal, Room, Message) {
+    function HomeCtrl($scope, $uibModal, Room, Message, $cookies) {
 
         this.rooms = Room.all;
+        this.currentUser = $cookies.get('blocChatCurrentUser');
+        this.currentRoom = null;
+
 
         this.open = function() {
             var modalInstance = $uibModal.open({
@@ -11,19 +14,26 @@
                 windowClass: 'modal-window'
             });
 
-        modalInstance.result.then(function(name) {
+            modalInstance.result.then(function(name) {
 				Room.add(name);
-			     });
+			});
         };
 
         this.setCurrentRoom = function(room) {
             this.currentRoom = room;
             this.messages = Message.getByRoomId(this.currentRoom.$id);
             };
-        }
 
+
+        this.sendMessage = function () {
+           this.newMessage.roomId = this.currentRoom.$id;
+           this.newMessage.username = this.currentUser;
+           Message.send(this.newMessage);
+       }
+
+    }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', '$uibModal', 'Room', 'Message', HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', '$uibModal', 'Room', 'Message', '$cookies', HomeCtrl]);
 })();
--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -4,15 +4,24 @@
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
 
+    Message.all = messages;
+
     Message.getByRoomId = function(roomId) {
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
     };
 
-    return Message;
+
+    Message.send = function(newMessage) {
+        newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+        messages.$add(newMessage);
+        console.log(newMessage);
     };
+
+    return Message;
+    }
 
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message]);
+    .factory('Message', ['$firebaseArray', '$cookies', Message]);
 })();
